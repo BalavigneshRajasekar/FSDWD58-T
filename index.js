fcc0042910c8c63e9d4ed30225a58a872d18d543
BalavigneshRajasekar <vigneshvickybsc1999@gmail.com>
2024-03-18 23:13:14 +0530

    New Files added

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-alert("hello")
+var data=7
+var flag=0
+for(var i=1;i<=data;i++){
+ if(data%i==0){
+ flag++
+ }
+}
+if(flag==2){
+console.log('yes')
+}else{
+    console.log('no')
+}
+console.log(flag)
