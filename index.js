2534b5d25f983e400bfaae5098394949860e6aed
BalavigneshRajasekar <vigneshvickybsc1999@gmail.com>
2024-03-19 20:57:43 +0530

    Day-2

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-var data=7
+var data=["7","1 2 3 4 5 6"]
+var question=data[1].split(' ')
 var flag=0
-for(var i=1;i<=data;i++){
- if(data%i==0){
- flag++
- }
+var flagA=0
+var index=0
+for(var i=0;i<question.length;i++){
+   if(i>0){
+         if(question[i-1]<question[i]){
+              flagA++
+         }else{
+            var value=i
+            index= value-1
+
+                flag++
+         }
+   }
+
 }
-if(flag==2){
-console.log('yes')
-}else{
-    console.log('no')
-}
-console.log(flag)
+
+
+if(flag==1){
+    console.log(index)
+    }else if(flag>1){
+     console.log(-1)
+    }else if(flagA==question.length-1){
+        console.log(-1)
+    }
+    
