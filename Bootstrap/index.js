3bc5da30a74804587ef820a216c24f145fcd5b4f
BalavigneshRajasekar <you@example.com>
2024-04-12 00:02:30 +0530

    Bootstrap

---
+++ Bootstrap/index.js
@@ -0,0 +1,11 @@
+let btn=document.querySelector('button')
+
+btn.addEventListener('click' , work)
+
+function work(){
+    let mobNav=document.getElementById('Mob')
+   
+   
+
+    mobNav.classList.toggle('MOb-Nav')
+}
