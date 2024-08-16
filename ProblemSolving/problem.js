651d55198b980b0ee10f78e3f2a4cc2355a3ce16
BalavigneshRajasekar <you@example.com>
2024-08-16 21:27:43 +0530

    new problem

--- ProblemSolving/problem.js
+++ ProblemSolving/problem.js
@@ -1,8 +1,27 @@
-const question = ["3 4"];
-const quest1 = question[0].split(" ");
-let ans = 1;
+const question = ["codekata"];
+const quest1 = question[0].split("");
+let quest2 = [];
+let flag = 0;
+const vowels = ["a", "e", "i", "o", "u"];
+let ans = [];
 
-for (let i = 1; i <= quest1[1]; i++) {
-  ans = ans * quest1[0];
+for (let i = 0; i < quest1.length; i++) {
+  flag = 0;
+  for (let j = 0; j < vowels.length; j++) {
+    if (quest1[i] !== vowels[j]) {
+      flag++;
+    }
+  }
+  if (flag == vowels.length) {
+    quest2.push(quest1[i]);
+  }
 }
-console.log(ans);
+
+if (quest2.length > 0) {
+  for (let i = quest2.length - 1; i >= 0; i--) {
+    ans.push(quest2[i]);
+  }
+  console.log(ans.join(""));
+} else {
+  console.log(-1);
+}
