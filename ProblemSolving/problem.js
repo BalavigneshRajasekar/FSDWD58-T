8edb71bd717f91252e1afdddf726b74d13935246
BalavigneshRajasekar <you@example.com>
2024-09-04 21:41:12 +0530

    new problem

--- ProblemSolving/problem.js
+++ ProblemSolving/problem.js
@@ -1,18 +1,9 @@
-const question = ["5 1", "3 2 2 3"];
+const question = ["5", "1 2 3 4 5"];
+let ans = 0;
+const n = question[1].split(" ");
 
-const n = question[0].split(" ");
-const k = question[1].split(" ");
-let flag = 0;
-for (let i = 0; i < n.length; i++) {
-  for (let j = 0; j < k.length; j++) {
-    if (n[i] == k[j]) {
-      flag = j + 1;
-    }
-  }
+for (let i = 0; i < n.length - 1; i++) {
+  ans = ans + Number(n[i]) + Number(n[i + 1]);
 }
 
-if (flag > 0) {
-  console.log(flag);
-} else {
-  console.log("-1");
-}
+console.log(ans);
